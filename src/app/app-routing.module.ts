1424f0f51137530c656a4220a4cd7061e669544c
Gerald-Star <ukwuomagerald@gmail.com>
2025-06-05 06:41:27 +0200

    setting the product route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './product/product-list/product-list.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: 'products', component: ProductListComponent}
+]
+
 
 
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
