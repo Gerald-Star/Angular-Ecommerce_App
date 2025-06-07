65354163487ae1714000b011875464d52832f723
Gerald-Star <ukwuomagerald@gmail.com>
2025-06-07 04:03:23 +0200

    setting up cart service models and mat-list from Angular materials for cart items

--- src/app/cart/cart-view/cart-view.component.ts
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-cart-view',
   templateUrl: './cart-view.component.html',
   styleUrls: ['./cart-view.component.css']
 })
-export class CartViewComponent {
+export class CartViewComponent implements OnInit {
 
+  cartItems: Product[] = [];
+
+
+  constructor(private cartService: CartService) { }
+
+  // Using ngOnInit lifecycle hook to fetch cart items
+  // Fetch cart items when the component initializes
+  ngOnInit(): void {
+    this.cartService.getCartItems().subscribe(data => {
+        this.cartItems = data;
+        //console.log('Cart items fetched successfully:', this.cartItems);
+      });
+   
+  }
 }
--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartViewComponent } from './cart-view/cart-view.component';
-
+import { MatCardModule } from '@angular/material/card'; // Assuming you have a cart module in Angular Material
+import { MatListModule } from '@angular/material/list'; // For displaying cart items in a list
+import { MatButtonModule } from '@angular/material/button'; // For cart actions like checkout or clear cart
+import { MatIconModule } from '@angular/material/icon'; // For icons in the cart view
 
 
 @NgModule({
@@ -9,7 +12,11 @@ import { CartViewComponent } from './cart-view/cart-view.component';
     CartViewComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class CartModule { }
